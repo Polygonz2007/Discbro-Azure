a9ebf2cb943693317b23f805d75d7ce962eb30bf
Polygonz <156954786+Polygonz2007@users.noreply.github.com>
2025-03-27 10:11:53 +0100

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Make directory for this stuff
+mkdir /app
+cd /app
+
+# Update and install stuff
 sudo apt-get update
 sudo apt install git nodejs npm -y
+
+# Get discbro
 git clone https://github.com/Polygonz2007/Discbro-Azure
-cd /home/poly/Discbro-Azure
+cd /discbro/Discbro-Azure
+
+# Install packages, run
 npm i
 sudo node .
